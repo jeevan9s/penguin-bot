6ef8fa4fd06e1c197000368652c31611312dffe1
Jeevan Sanchez <jeevansanchez42@gmail.com>
2026-07-31 15:57:09 -0400

    replace test data in dashboard prox view

--- dashboard/src/Dashboard.tsx
+++ dashboard/src/Dashboard.tsx
@@ -100,9 +100,9 @@ export default function Dashboard() {
 
         {activeView === "proximity"  && (
           <ProximityView
-            distL={12}
-            distM={2}
-            distR={50}
+            distL={penguinData.sensL.proximity}
+            distM={penguinData.sensM.proximity}
+            distR={penguinData.sensR.proximity}
             obstacleDetected={penguinData.obsDetected}
             activeView={activeView}
             onSelectBoard={() => setActiveView("board")}
